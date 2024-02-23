6cc7d462e29e9b28bae3f34e7c41583d104bdd75
Gaben <99161297+1337kat@users.noreply.github.com>
2024-02-23 10:34:00 -0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,36 @@
+-- No collide
+--Thread1
+local Thread1 = coroutine.create(function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1337kat/o/main/keyheads.lua'))()
+end)
+coroutine.resume(Thread1)
+
+--Thread4
+local Thread5 = coroutine.create(function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1337kat/l/main/Fullbright.lua'))()
+end)
+coroutine.resume(Thread5)
+
+--Thread7
+local Thread7 = coroutine.create(function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1337kat/l/main/Crosshair.lua'))()
+end)
+coroutine.resume(Thread7)
+
+--Thread8
+local Thread8 = coroutine.create(function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1337kat/l/main/V%20neck.lua'))()
+end)
+coroutine.resume(Thread8)
+
+--Thread9
+local Thread9 = coroutine.create(function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+coroutine.resume(Thread9)
+
+--Thread10
+local Thread10 = coroutine.create(function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1337kat/l/main/Armour%20Indicator.lua'))()
+end)
+coroutine.resume(Thread10)
